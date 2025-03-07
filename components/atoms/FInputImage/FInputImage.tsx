91aa09ee78ffc642ec7ded08289fe73627c755d6
Gustavo Martins <gustavomartins.developer@gmail.com>
2025-03-07 00:55:00 -0300

    Only images allowed

--- components/atoms/FInputImage/FInputImage.tsx
+++ components/atoms/FInputImage/FInputImage.tsx
@@ -9,7 +9,7 @@ interface FInputImageProps {
 export default function FInputImage(props: FInputImageProps) {
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ['images', 'videos'],
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
