8fed9e45784976eae9bf217a1a1eb7b1d6f53980
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-03-14 15:56:01 -0300

    feat: add Transaction context

---
+++ context/TransactionContext.tsx
@@ -0,0 +1,60 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { Transaction } from '@/firebase/types/transaction';
+import { useAuth } from './AuthContext';
+import { getTransactions } from '@/firebase/controllers/transaction';
+import { useAccount } from './AccountContext';
+
+interface TransactionContextType {
+  transactions: Transaction[];
+  loading: boolean;
+  fetchTransactions: () => Promise<void>;
+}
+
+const TransactionContext = createContext<TransactionContextType | undefined>(
+  undefined
+);
+
+export const TransactionProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const { user } = useAuth();
+  const { account } = useAccount();
+
+  const fetchTransactions = async () => {
+    if (!user || !account) return;
+
+    setLoading(true);
+    try {
+      const fetchedTransactions = await getTransactions(user.uid, account.id);
+
+      setTransactions(fetchedTransactions);
+    } catch (error) {
+      console.error('Error fetching transactions:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <TransactionContext.Provider
+      value={{ transactions, loading, fetchTransactions }}
+    >
+      {children}
+    </TransactionContext.Provider>
+  );
+};
+
+export const useTransactions = (): TransactionContextType => {
+  const context = useContext(TransactionContext);
+
+  if (!context) {
+    throw new Error(
+      'useTransactions must be used within a TransactionProvider'
+    );
+  }
+  return context;
+};
