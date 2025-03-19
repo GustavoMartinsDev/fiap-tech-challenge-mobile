994ff66b1b0ef478d5e00d5063243754855635b3
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-03-18 23:45:46 -0300

    fix: tab icons on android

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -5,14 +5,10 @@ import { SymbolWeight } from 'expo-symbols';
 import React from 'react';
 import { OpaqueColorValue, StyleProp, ViewStyle } from 'react-native';
 
-// Add your SFSymbol to MaterialIcons mappings here.
 const MAPPING = {
   // See MaterialIcons here: https://icons.expo.fyi
-  // See SF Symbols in the SF Symbols app on Mac.
-  'house.fill': 'home',
-  'paperplane.fill': 'send',
-  'chevron.left.forwardslash.chevron.right': 'code',
-  'chevron.right': 'chevron-right',
+  house: 'home',
+  'wallet.bifold': 'account-balance-wallet',
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
@@ -22,11 +18,6 @@ const MAPPING = {
 
 export type IconSymbolName = keyof typeof MAPPING;
 
-/**
- * An icon component that uses native SFSymbols on iOS, and MaterialIcons on Android and web. This ensures a consistent look across platforms, and optimal resource usage.
- *
- * Icon `name`s are based on SFSymbols and require manual mapping to MaterialIcons.
- */
 export function IconSymbol({
   name,
   size = 24,
